8c0b74b2e58256f501b906b497ea195aed8764dd
Horiuchi_H <horiuchi.g@gmail.com>
2015-05-28 16:18:11 +0900

    add `background` property

--- src/imagemagick.cc
+++ src/imagemagick.cc
@@ -87,6 +87,7 @@ struct convert_im_ctx : im_ctx_base {
     std::string format;
     std::string filter;
     std::string blur;
+	std::string background;
     unsigned int quality;
     int rotate;
     int density;
@@ -180,6 +181,25 @@ void DoConvert(uv_work_t* req) {
     Magick::Blob srcBlob( context->srcData, context->length );
 
     Magick::Image image;
+
+    if ( ! context->background.empty() ) {
+        const char* background = context->background.c_str();
+        try {
+            Magick::Color bg(background);
+            image.backgroundColor(bg);
+            if (debug) printf( "set background: %s\n", background );
+        }
+        catch (std::exception& err) {
+            std::string message = "image.backgroundColor failed with error: ";
+            message            += err.what();
+            context->error = message;
+            return;
+        }
+        catch (...) {
+            context->error = std::string("unhandled error");
+            return;
+        }
+    }
 
     if ( !ReadImageMagick(&image, srcBlob, context->srcFormat, context) )
         return;
@@ -557,6 +577,10 @@ NAN_METHOD(Convert) {
     Local<Value> filterValue = obj->Get( Nan::New<String>("filter").ToLocalChecked() );
     context->filter = !filterValue->IsUndefined() ?
         *String::Utf8Value(filterValue) : "";
+
+    Local<Value> backgroundValue = obj->Get( NanNew<String>("background") );
+    context->background = !backgroundValue->IsUndefined() ?
+        *NanAsciiString(backgroundValue) : "";
 
     uv_work_t* req = new uv_work_t();
     req->data = context;
